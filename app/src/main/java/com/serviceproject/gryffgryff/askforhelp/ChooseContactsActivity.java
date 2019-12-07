8f83ded1b6e44a23c39caf520d6e566bb1665370
GryffGryff <elenanisha@gmail.com>
2019-12-06 17:19:04 -0800

    working version of the app with the ability to send messages to a single person without opening the text messaging app. Also just added a settings activity which is not yet functional

---
+++ app/src/main/java/com/serviceproject/gryffgryff/askforhelp/ChooseContactsActivity.java
@@ -0,0 +1,121 @@
+package com.serviceproject.gryffgryff.askforhelp;
+
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class ChooseContactsActivity extends AppCompatActivity {
+
+    Button firstCG;
+    Button secondCG;
+    Button thirdCG;
+    Button fourthCG;
+    Button back;
+    Bundle whatToText;
+    Bundle whoToText = new Bundle();
+
+    //int requestCode = 2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_contacts);
+
+        setVariables();
+        addNumbersToBundle();
+        setClickListener();
+    }
+
+    public void setVariables() {
+        firstCG = (Button) findViewById(R.id.firstContactGroup);
+        secondCG = (Button) findViewById(R.id.secondContactGroup);
+        thirdCG = (Button) findViewById(R.id.thirdContactGroup);
+        fourthCG = (Button) findViewById(R.id.fourthContactGroup);
+        back = (Button) findViewById(R.id.backButtonOne);
+        whatToText = getIntent().getExtras();
+    }
+
+    public void addNumbersToBundle() {
+        whoToText.putString("first_group", "4128777232, 4128777338");
+        whoToText.putString("second_group", "4128779326");
+        whoToText.putString("third_group", "");
+        whoToText.putString("fourth_group", "");
+    }
+
+    public void setClickListener() {
+        firstCG.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //startText("first_group");
+                textPerson("first_group");
+            }
+        });
+
+        secondCG.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //startText("second_group");
+                textPerson("second_group");
+            }
+        });
+
+        thirdCG.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //startText("third_group");
+                textPerson("third_group");
+            }
+        });
+
+        fourthCG.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //startText("fourth_group");
+                textPerson("fourth_group");
+            }
+        });
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ChooseContactsActivity.this, ChooseRequestsActivity.class);
+                ChooseContactsActivity.this.startActivity(intent);
+            }
+        });
+    }
+
+    public void startText(String text) {
+        try {
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse("smsto:"));
+            intent.setType("vnd.android-dir/mms-sms");
+            intent.putExtra("sms_body", whatToText.getString(whatToText.getString("last_button_pressed")));
+            //intent.putExtra("address", new String("4128779326"));
+            intent.putExtra("address", new String(whoToText.getString(text)));
+            startActivity(intent);
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "SMS failed, please try again later!", Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        }
+    }
+
+    public void textPerson(String text) {
+
+        String address = (whoToText.getString(text));
+        String textBody = (whatToText.getString(whatToText.getString("last_button_pressed")));
+        String scAddress = null;
+        PendingIntent sentIntent = null;
+        PendingIntent deliveryIntent = null;
+
+        SmsManager smsManager = SmsManager.getDefault();
+        smsManager.sendTextMessage(address, scAddress, textBody, sentIntent, deliveryIntent);
+
+        Toast.makeText(ChooseContactsActivity.this, "Text sent", Toast.LENGTH_LONG).show();
+    }
+}
---
+++ app/src/main/java/com/serviceproject/gryffgryff/askforhelp/ChooseRequestsActivity.java
@@ -0,0 +1,104 @@
+package com.serviceproject.gryffgryff.askforhelp;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class ChooseRequestsActivity extends AppCompatActivity {
+
+    Button firstButton;
+    Button secondButton;
+    Button thirdButton;
+    Button fourthButton;
+    Button settings;
+
+    Bundle bundle = new Bundle();
+
+    Integer requestCode = 3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_requests);
+        setVariables();
+        addNamesToBundle();
+        getPermission();
+        setClickListener();
+    }
+
+    public void setVariables() {
+        firstButton = (Button) findViewById(R.id.firstRequest);
+        secondButton = (Button) findViewById(R.id.secondRequest);
+        thirdButton = (Button) findViewById(R.id.thirdRequest);
+        fourthButton = (Button) findViewById(R.id.fourthRequest);
+        settings = (Button) findViewById(R.id.settings);
+    }
+
+    public void addNamesToBundle() {
+        bundle.putString("first_text", getButtonName(firstButton));
+        bundle.putString("second_text", getButtonName(secondButton));
+        bundle.putString("third_text", getButtonName(thirdButton));
+        bundle.putString("fourth_text", getButtonName(fourthButton));
+        bundle.putString("last_button_pressed", "none");
+    }
+
+    public void setClickListener() {
+        firstButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startIntent("first_text");
+            }
+        });
+        secondButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startIntent("second_text");
+            }
+        });
+        thirdButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startIntent("third_text");
+            }
+        });
+        fourthButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startIntent("fourth_text");
+            }
+        });
+        settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
+    }
+
+    public String getButtonName(Button currButton) {
+        return currButton.getText().toString();
+    }
+
+    public void startIntent(String text) {
+        Intent intent = new Intent(ChooseRequestsActivity.this, ChooseContactsActivity.class);
+        bundle.remove("last_button_pressed");
+        bundle.putString("last_button_pressed", text);
+        intent.putExtras(bundle);
+        ChooseRequestsActivity.this.startActivity(intent);
+    }
+
+    public void getPermission() {
+        if (ContextCompat.checkSelfPermission(ChooseRequestsActivity.this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(ChooseRequestsActivity.this, new String[] {Manifest.permission.SEND_SMS}, requestCode );
+            onRequestPermissionsResult(requestCode, new String[] {Manifest.permission.SEND_SMS}, new int[] {PackageManager.PERMISSION_GRANTED});
+        } else {
+            //permission granted
+            //Toast.makeText(ChooseRequestsActivity.this, "permission already granted", Toast.LENGTH_LONG).show();
+        }
+    }
+}
